caf5813b493482a18cec2bf58b4187b3b07cd0ca
Yvan Razafindramanana <yvan.razafindramanana@softeam.fr>
2017-05-10 22:52:22 +0200

    Cleanup, fix logs and missing properties

--- src/main/java/net/ludeo/recikligi/AppConfig.java
+++ src/main/java/net/ludeo/recikligi/AppConfig.java
@@ -2,15 +2,25 @@ package net.ludeo.recikligi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = "net.ludeo.recikligi")
+@PropertySource("classpath:recikligi.properties")
 @EnableAutoConfiguration
 public class AppConfig {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AppConfig.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer
+    propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
--- src/main/java/net/ludeo/recikligi/HomeController.java
+++ src/main/java/net/ludeo/recikligi/HomeController.java
@@ -1,13 +1,22 @@
 package net.ludeo.recikligi;
 
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Setter
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public String home() {
-		return "home";
-	}
+    @Value("${appTitle}")
+    private String appTitle;
+
+    @RequestMapping("/")
+    public String home(final Model model) {
+        model.addAttribute("appTitle", appTitle);
+
+        return "camera";
+    }
 }
