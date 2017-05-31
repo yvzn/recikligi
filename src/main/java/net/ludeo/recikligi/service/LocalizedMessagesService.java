e04e3bd1edf56f96fc04926c94c4d487f3af2fa0
Yvan Razafindramanana <yvan.razafindramanana@softeam.fr>
2017-05-31 16:06:08 +0200

    Switched to lombok

--- src/main/java/net/ludeo/recikligi/service/LocalizedMessagesService.java
+++ src/main/java/net/ludeo/recikligi/service/LocalizedMessagesService.java
@@ -1,17 +1,14 @@
 package net.ludeo.recikligi.service;
 
+import lombok.Setter;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.context.i18n.LocaleContextHolder;
 
-public class LocalizedMessagesService implements MessageSourceAware {
+@Setter
+class LocalizedMessagesService implements MessageSourceAware {
 
     private MessageSource messageSource;
-
-    @Override
-    public void setMessageSource(final MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
 
     String getMessage(final String code, final Object... args) {
         return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
