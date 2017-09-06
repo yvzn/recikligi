99e1564fbd3b3c1b958a4cf45d30d663c6cfe40e
Yvan Razafindramanana <yvan.razafindramanana@softeam.fr>
2017-09-06 14:18:09 +0200

    Display error stacktraces

--- src/main/java/net/ludeo/recikligi/controller/ControllerExceptionHandler.java
+++ src/main/java/net/ludeo/recikligi/controller/ControllerExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 
 @ControllerAdvice
@@ -18,8 +21,21 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Throwable.class)
     public String handleException(final Throwable throwable, final Model model) {
         String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+        String errorStracktrace = getStacktrace(throwable);
+
         model.addAttribute("error", errorMessage);
         model.addAttribute("status", HttpURLConnection.HTTP_INTERNAL_ERROR);
+        model.addAttribute("stacktrace", errorStracktrace);
+
         return "error";
+    }
+
+    private String getStacktrace(Throwable throwable) {
+        try (StringWriter sw = new StringWriter(); PrintWriter pw = new PrintWriter(sw)) {
+            throwable.printStackTrace(pw);
+            return sw.toString();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 }
