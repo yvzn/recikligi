68bacee087db3454b63f864098954b58aedca123
Yvan Razafindramanana <yvan.razafindramanana@softeam.fr>
2017-09-06 12:43:27 +0200

    Merged simple controllers into a single one

--- src/main/java/net/ludeo/recikligi/controller/DefaultController.java
+++ src/main/java/net/ludeo/recikligi/controller/DefaultController.java
@@ -1,13 +1,24 @@
 package net.ludeo.recikligi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class DefaultController {
 
     @RequestMapping("/")
     public String home() {
         return "redirect:/camera";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/limit")
+    public String limit() {
+        return "limit";
     }
 }
