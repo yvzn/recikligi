12dae5db2c4ea1c2d2578c12fea09101ef7e3c56
Yvan Razafindramanana <mail@ludeo.net>
2021-12-15 07:36:28 +0100

    Enable daily limit filter for Azure vision too

--- src/main/java/net/ludeo/recikligi/AppConfig.java
+++ src/main/java/net/ludeo/recikligi/AppConfig.java
@@ -1,6 +1,22 @@
+/*
+   Copyright 2017-2021 Yvan Razafindramanana
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package net.ludeo.recikligi;
 
 import net.ludeo.recikligi.controller.DailyUsageLimitFilter;
+import net.ludeo.recikligi.service.recognition.AzureVisualRecognitionService;
 import net.ludeo.recikligi.service.recognition.WatsonVisualRecognitionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -21,7 +37,7 @@ import java.util.Arrays;
 @EnableAutoConfiguration
 public class AppConfig {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
     }
 
@@ -34,9 +50,17 @@ public class AppConfig {
 
     @Bean
     @ConditionalOnBean(WatsonVisualRecognitionService.class)
-    public FilterRegistrationBean<DailyUsageLimitFilter> registerDailyUsageLimitIfWatsonServiceIsEnabled(DailyUsageLimitFilter filter) {
+    public FilterRegistrationBean<DailyUsageLimitFilter> registerDailyUsageLimitIfWatsonVisualRecognition(DailyUsageLimitFilter filter) {
         FilterRegistrationBean<DailyUsageLimitFilter> registration = new FilterRegistrationBean<>(filter);
         registration.setUrlPatterns(Arrays.asList("/camera", "/recyclable/*"));
         return registration;
     }
+
+	@Bean
+	@ConditionalOnBean(AzureVisualRecognitionService.class)
+	public FilterRegistrationBean<DailyUsageLimitFilter> registerDailyUsageLimitIfAzureVisualRecognitionService(DailyUsageLimitFilter filter) {
+		FilterRegistrationBean<DailyUsageLimitFilter> registration = new FilterRegistrationBean<>(filter);
+		registration.setUrlPatterns(Arrays.asList("/camera", "/recyclable/*"));
+		return registration;
+	}
 }
